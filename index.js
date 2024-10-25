e929bcc0bce66ddfaaabd03468c5ae817c4117a4
unknown <bhanuprasad08>
2024-10-26 01:59:50 +0530

    v4

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
-import bcrypt, { hash } from "bcrypt"
+import jwt from "jsonwebtoken"
+import axios from "axios"
+import bcrypt from "bcrypt"
 import connnetDB from "./db/index.js"
 import User from "./models/user.models.js"
 import Data from "./models/data.models.js"
-import Group from "./models/group.models.js"
-import GroupMembers from "./models/groupMembers.js"
-import GroupMembersData from "./models/groupMembersData.models.js"
+
 
 const app = express()
 
@@ -63,14 +63,17 @@ app.post("/signup", async (req, res) => {
     const { name, email, password } = req.body
 
     const userCheck = await User.findOne({ email })
-
-    if (userCheck) {
-      return res
-        .status(409)
-        .json({ message: "User already exists with this email" })
-    }
     const salt = 10
     const hashedPassword = await bcrypt.hash(password, salt)
+
+    if (userCheck) {
+      if(!password){
+        return res.status(400).json({ message: "Password is required for existing users." });
+      }
+
+      await User.updateOne({ email }, { password: hashedPassword });
+      return res.status(200).json({ message: "Password updated successfully." });
+    }
 
     const user = new User({ name, email, password: hashedPassword })
     await user.save()
@@ -82,6 +85,38 @@ app.post("/signup", async (req, res) => {
     })
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" })
+  }
+})
+
+//to google signup
+app.post("/googleSignup", async (req, res) => {
+  const { token, email, name } = req.body
+
+  try {
+    const googleResponse = await axios.get(
+      `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`
+    )
+    if (googleResponse.data.email !== email) {
+      return res.status(400).json({ message: "Token email mismatch" })
+    }
+
+    const checkUser = await User.findOne({ email })
+    if (checkUser) {
+      return res.status(409).json({ message: "User already exists" })
+    }
+    const newUser = new User({
+      email: email,
+      name: name,
+    })
+    await newUser.save()
+
+    const jwtToken = jwt.sign({ email, name }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    })
+
+    return res.status(200).json({ token: jwtToken })
+  } catch (error) {
+    return res.status(500).json({ message: "Error signing up" })
   }
 })
 
@@ -106,6 +141,26 @@ app.post("/login", async (req, res) => {
     })
   } catch (error) {
     return res.status(500).json({ message: `error while login: ${error}` })
+  }
+})
+
+//to login with google
+
+app.post("/googleLogin", async (req, res) => {
+  const { email } = req.body
+
+  try {
+    const emailCheck = await User.findOne({ email })
+    if (!emailCheck) {
+      return res.status(404).json({ message: "Email not found" })
+    }
+    return res.status(200).json({
+      message: "Successfully logged in",
+      userId: emailCheck._id,
+      name: emailCheck.name,
+    })
+  } catch (error) {
+    return res.status(500).json({ message: `error while login ${error}` })
   }
 })
 
@@ -143,7 +198,7 @@ app.post("/users/:userId/data", async (req, res) => {
 
     if (dataRecord) {
       dataRecord.amount += amount
-      dataRecord.history.push({amount: amount, date: new Date()})
+      dataRecord.history.push({ amount: amount, date: new Date() })
       await dataRecord.save()
 
       return res.status(200).json({
@@ -256,253 +311,3 @@ app.delete("/users/:userId/data/:dataId", async (req, res) => {
   }
 })
 
-/*----------------------Group Box--------------------------*/
-
-// To create Group
-app.post("/create-group", async (req, res) => {
-  const { groupName, groupPassword } = req.body
-
-  const existingGroup = await Group.findOne({ groupName })
-  console.log(existingGroup)
-  try {
-    if (existingGroup) {
-      return res.status(409).json({ message: "Group name already exists" })
-    }
-
-    const salt = 10
-    const hashedGroupPass = await bcrypt.hash(groupPassword, salt)
-
-    const newGroup = await Group.create({
-      groupName: groupName,
-      groupPassword: hashedGroupPass,
-    })
-
-    await newGroup.save()
-
-    return res
-      .status(201)
-      .json({ message: "Group created successfully", group: newGroup })
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "getting error while creating group: " + error.message })
-  }
-})
-
-// To Join Group
-app.post("/join-group", async (req, res) => {
-  const { groupName, groupPassword, groupMembers, groupMembersPassword } =
-    req.body
-
-  try {
-    const salt = 10
-
-    const existingGroup = await Group.findOne({ groupName })
-    if (!existingGroup) {
-      return res.status(404).json({ message: "No group found" })
-    }
-
-    const checkGroupPass = await bcrypt.compare(
-      groupPassword,
-      existingGroup.groupPassword
-    )
-    if (!checkGroupPass) {
-      return res.status(401).json({ message: "Entered wrong Group Password" })
-    }
-
-    const existingMem = await GroupMembers.findOne({
-      members: groupMembers,
-      groupId: existingGroup._id,
-    })
-
-    if (existingMem) {
-      const existingMemPassword = await bcrypt.compare(
-        groupMembersPassword,
-        existingMem.password
-      )
-      if (existingMemPassword) {
-        return res.status(200).json({
-          message: "User Joined Group",
-          userId: existingMem._id,
-          userName: groupMembers,
-          spents: existingMem.spents,
-          groupName: existingGroup.groupName,
-          groupId: existingGroup._id,
-        })
-      }
-      return res.status(401).json({ message: "Entered wrong User Password" })
-    } else {
-      const hashedGroupMemberPassword = await bcrypt.hash(
-        groupMembersPassword,
-        salt
-      )
-
-      const newUser = new GroupMembers({
-        members: groupMembers,
-        password: hashedGroupMemberPassword,
-        spents: 0,
-        groupId: existingGroup._id,
-      })
-
-      await newUser.save()
-
-      existingGroup.groupMembers.push(newUser._id)
-      await existingGroup.save()
-      return res.status(201).json({
-        message: "User created and joined successfully",
-        userId: newUser._id,
-        userName: groupMembers,
-        spents: newUser.spents,
-      })
-    }
-  } catch (error) {
-    console.error("An error: ", error)
-    return res.status(500).json({ error: "While joining group" })
-  }
-})
-
-// To get all groups
-app.get("/groups", async (req, res) => {
-  try {
-    const groups = await Group.find()
-    return res.status(200).json({ groups })
-  } catch (error) {
-    return res.status(404).json({ error: "Error while getting details" })
-  }
-})
-
-//To post data into group by member
-app.post("/:groupId/members/:memberId/data", async (req, res) => {
-  const { category, amount } = req.body
-  const { memberId, groupId } = req.params
-
-  try {
-    const existingGroup = await Group.findById(groupId)
-    if (!existingGroup) {
-      return res.status(404).json({ message: "Group not found" })
-    }
-    const existingGroupMember = await GroupMembers.findOne({
-      _id: memberId,
-      groupId: groupId,
-    })
-    if (!existingGroupMember) {
-      return res.status(404).json({ message: "Member not found in the group" })
-    }
-
-    const existingCategory = await GroupMembersData.findOne({
-      groupMember: memberId,
-      groupId: groupId,
-      category: category,
-    })
-
-    if (existingCategory) {
-      existingCategory.amount += amount
-      await existingCategory.save()
-
-      existingGroupMember.spents += amount
-      await existingGroupMember.save()
-
-      return res.status(200).json({
-        message: "Amount added successfully",
-        category: existingCategory,
-        amount: amount,
-        spents: existingGroupMember.spents,
-      })
-    }
-
-    const newCategory = new GroupMembersData({
-      groupMember: memberId,
-      groupId: groupId,
-      category: category,
-      amount: amount,
-    })
-    await newCategory.save()
-
-    existingGroupMember.spents += amount
-    await existingGroupMember.save()
-
-    return res.status(200).json({
-      message: "New category added successfully",
-      category: newCategory,
-      amount: amount,
-    })
-  } catch (error) {
-    return res.status(500).json({ error: "Error while posting details" })
-  }
-})
-
-//To get the data of all users
-app.get("/:groupId/members/data", async (req, res) => {
-  const { groupId } = req.params
-  try {
-    const existingGroup = await Group.findById(groupId).populate("groupMembers")
-
-    if (!existingGroup) {
-      return res.status(404).json({ message: "Group not found" })
-    }
-
-    const members = existingGroup.groupMembers
-    if (!members) {
-      return res.status(404).json({ message: "No members found in this group" })
-    }
-
-    let groupMembersData = []
-    for (let i = 0; i < members.length; i++) {
-      const dataByGroupMember = await GroupMembersData.find({
-        groupMember: members[i]._id,
-      })
-
-      groupMembersData.push({
-        memberId: members[i]._id,
-        memberName: members[i].members,
-        spents: members[i].spents,
-        dataByGroupMember,
-      })
-    }
-    return res.status(200).json({
-      membersData: groupMembersData,
-    })
-  } catch (error) {
-    console.error("error retrieving members", error)
-    return res
-      .status(500)
-      .json({ error: "Error while getting data of members" })
-  }
-})
-//To Delete Data of particular member
-
-app.delete("/:groupId/members/:memberId/data/:dataId", async (req, res) => {
-  const { groupId, memberId, dataId } = req.params
-
-  try {
-    const existingGroup = await Group.findById(groupId)
-    if (!existingGroup) {
-      return res.status(404).json({ message: "Group not found" })
-    }
-    const existingMem = await GroupMembers.findOne({
-      _id: memberId,
-      groupId: groupId,
-    })
-    if (!existingMem) {
-      return res.status(404).json({ message: "Member not found in the group" })
-    }
-    const existingCategory = await GroupMembersData.findOne({
-      _id: dataId,
-      groupMember: memberId,
-      groupId: groupId,
-    })
-    if (!existingCategory) {
-      return res
-        .status(404)
-        .json({ message: "Data not found in the member's category" })
-    }
-
-    await GroupMembersData.deleteOne({ _id: dataId })
-
-    return res.status(200).json({ message: "Data deleted successfully" })
-  } catch (error) {
-    return res
-      .status(404)
-      .json({ message: "Getting error while deleting category" })
-  }
-})
